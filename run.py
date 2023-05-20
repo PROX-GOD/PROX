10094bc61597333b59ae247682b8f8808a93faef
PROX-GOD <117366207+PROX-GOD@users.noreply.github.com>
2023-05-20 16:27:16 +0545

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+import os, sys, platform,time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    print("[+] FOLLOW PAGE FOR NEW CMD AND COOKIES")
+os.system('xdg-open https://www.facebook.com/profile.php?id=100063463910453')
+os.system('xdg-open https://www.facebook.com/PR3SH4KBAUF33LTH3POW3R');time.sleep(5)
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import DUMP64
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import DUMP32

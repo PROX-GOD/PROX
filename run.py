4111d7e4b76d77bff2fab9c9b59fde368f6e1995
PROX-GOD <117366207+PROX-GOD@users.noreply.github.com>
2023-05-20 16:39:42 +0545

    Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ except:
     os.system('pip install requests')
     print("[+] FOLLOW PAGE FOR NEW CMD AND COOKIES")
 os.system('xdg-open https://www.facebook.com/profile.php?id=100063463910453')
-os.system('xdg-open https://www.facebook.com/PR3SH4KBAUF33LTH3POW3R');time.sleep(5)
+os.system('xdg-open https://www.facebook.com/PR3SH4KBAUF33LTH3POW3R');time.sleep(2)
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    import DUMP64
+    import DUMPP
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')
